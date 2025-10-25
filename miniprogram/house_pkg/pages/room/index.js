e00378c2affac715856bc4a2100400b6c4a3294d
wehome-h <375702088@qq.com>
2025-10-25 16:23:27 +0800

    优化房间页面，动态生成房间信息并更新链接

--- miniprogram/house_pkg/pages/room/index.js
+++ miniprogram/house_pkg/pages/room/index.js
@@ -1,66 +1,71 @@
 // house_pkg/pages/room/index.ts
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
+    point: '',
+    building: '',
+    rooms: []
+  },
 
+  fake(point, building) {
+    const size = Math.floor(Math.random() * 5) + 4
+    const rooms = []
+    for (let i = 0; i < size; i++) {
+      const floor = Math.floor(Math.random() * 19) + 1
+      const no = Math.floor(Math.random() * 2) + 1
+      const room = [floor, 0, no].join('')
+      if (rooms.includes(room)) return
+      rooms.push(room)
+    }
+    this.setData({
+      rooms,
+      point,
+      building
+    })
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad() {
-
+  onLoad(options) {
+    const { point, building } = options
+    this.fake(point, building)
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady() {
-
-  },
+  onReady() {},
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow() {
-
-  },
+  onShow() {},
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide() {
-
-  },
+  onHide() {},
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload() {
-
-  },
+  onUnload() {},
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh() {
-
-  },
+  onPullDownRefresh() {},
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom() {
-
-  },
+  onReachBottom() {},
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage() {
-
-  }
+  onShareAppMessage() {}
 })
