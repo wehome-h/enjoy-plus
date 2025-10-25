1479812af73185ed0fb8de83e72a4e48dd99075f
wehome-h <375702088@qq.com>
2025-10-25 16:02:14 +0800

    添加位置搜索和逆地址解析功能，优化重新定位逻辑

--- miniprogram/house_pkg/pages/locate/index.js
+++ miniprogram/house_pkg/pages/locate/index.js
@@ -1,66 +1,103 @@
-// house_pkg/pages/locate/index.ts
-Page({
+import QQMapWX from '../../../libs/qqmap-wx-jssdk.min.js'
 
+// 创建 qqmapsdk 实例
+const qqmapsdk = new QQMapWX({
+  key: 'BVEBZ-GJZLL-F7UPG-EI4IJ-JZFV2-ZVFKJ'
+})
+
+Page({
   /**
    * 页面的初始数据
    */
   data: {
+    list: [],
+    address: ''
+  },
 
+  search(latitude, longitude) {
+    qqmapsdk.search({
+      location: {
+        latitude,
+        longitude
+      },
+      keyword: '住宅小区',
+      page_size: 5,
+      success: (res) => {
+        this.setData({
+          list: res.data
+        })
+      }
+    })
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad() {
+  async onLoad() {
+    const res = await wx.getLocation({ type: 'gcj02' })
+    console.log('当前位置信息：', res)
+    this.getPoint(res.latitude, res.longitude)
+    this.search(res.latitude, res.longitude)
+  },
 
+  /** 逆地址解析 */
+  getPoint(latitude, longitude) {
+    qqmapsdk.reverseGeocoder({
+      location: {
+        latitude,
+        longitude
+      },
+      success: (res) => {
+        console.log('逆地址解析结果：', res)
+        this.setData({
+          address: res.result.address
+        })
+      }
+    })
+  },
+
+  /** 重新定位 */
+  async reLocation() {
+    const res = await wx.chooseLocation()
+    console.log('重新选择位置：', res)
+    this.setData({
+      address: res.address
+    })
+    this.search(res.latitude, res.longitude)
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady() {
-
-  },
+  onReady() {},
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow() {
-
-  },
+  onShow() {},
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide() {
-
-  },
+  onHide() {},
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload() {
-
-  },
+  onUnload() {},
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh() {
-
-  },
+  onPullDownRefresh() {},
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom() {
-
-  },
+  onReachBottom() {},
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage() {
-
-  }
+  onShareAppMessage() {}
 })
